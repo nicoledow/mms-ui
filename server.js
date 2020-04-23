319d7d6226cd37e2da4f2b64abbfba120d09a945
Nicole Dow <nikki.dow71@gmail.com>
2020-04-23 08:32:16 -0700

    fix server filepaths

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3001;
+const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+ });
