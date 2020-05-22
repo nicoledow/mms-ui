0d1f6ecc8cd78afa767380240dd945af9d807611
Nicole Dow <nikki.dow71@gmail.com>
2020-05-21 20:41:06 -0700

    express server

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// const path = require('path');
-// const fs = require('fs');
-// const express = require('express');
-// const app = express();
-// const publicPath = path.join('public');
-// const port = process.env.PORT || 3001;
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
 
-// app.use(express.static('./build'));
+app.use(express.static('./build'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(__dirname + '/build/index.html');
-//  });
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+ });
 
-// app.listen(port, () => {
-//     console.log(`Server is up on port ${port}!`);
-//  });
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+ });
