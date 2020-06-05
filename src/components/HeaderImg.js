cc9574493bf5bf4c2a0067ce8863a04b61243713
Nicole Dow <nikki.dow71@gmail.com>
2020-06-04 20:25:41 -0700

    get rido of hero logo above logo on small screens

--- src/components/HeaderImg.js
+++ src/components/HeaderImg.js
@@ -2,5 +2,5 @@ import React from 'react';
 import logo1 from '../images/logo1.png';
 
 export default function HeaderImg() {
-    return <img src={logo1} alt="Logo" className="header__logo" />
+    return <img src={logo1} alt="Logo" className="header__logo--large" />
 }
