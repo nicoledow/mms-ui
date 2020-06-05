79a31881356f41ac6566c8df6374cc5fe47aff40
Nicole Dow <nikki.dow71@gmail.com>
2020-06-04 20:17:57 -0700

    different logos for different screen sizes, fix animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Logo from './logo.png';
+import Logo1 from '../images/logo1.png';
+import Logo2 from '../images/logo2.png';
 
 export default class Header extends React.Component {
     constructor(){
@@ -12,7 +13,8 @@ export default class Header extends React.Component {
     render() {
         return (
             <div className="header">
-                <img src={Logo} alt="Logo" className="header__logo" />
+                <img src={Logo1} alt="Logo" className="header__logo--large" />
+                <img src={Logo2} alt="Logo" className="header__logo--small" />
                 <p className="header__p">Answer a few simple questions to get your estimate.</p>
             <button className="header__btn"><Link to="/form">Get Started</Link></button>
         </div>
--- src/components/HeaderImg.js
+++ src/components/HeaderImg.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.png';
+import logo1 from '../images/logo1.png';
 
 export default function HeaderImg() {
-    return <img src={logo} alt="Logo" className="header__logo" />
+    return <img src={logo1} alt="Logo" className="header__logo" />
 }
