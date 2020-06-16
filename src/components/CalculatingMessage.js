b92c3a58a1c4c49c095dad6de3f73ab94cb797a2
Nicole Dow <nikki.dow71@gmail.com>
2020-06-15 18:27:20 -0700

    add loading spinner

--- src/components/CalculatingMessage.js
+++ src/components/CalculatingMessage.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import getEstimate from '../actions/getEstimate';
 import { connect } from 'react-redux';
+import { css } from '@emotion/core';
+import MoonLoader from 'react-spinners/MoonLoader';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 class CalculatingMessage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            loading: true
+        };
+    }
 
     componentDidMount() {
         this.props.sendDataForEstimate(this.props.data);
@@ -10,8 +24,8 @@ class CalculatingMessage extends React.Component {
 
     render() {
         return (
-            <div className="formDiv">
-                <h3 className="header__p">Calculating your estimate...</h3>
+            <div className="formDiv sweet-loading">
+                <MoonLoader css={override} size={150} color="silver" loading={this.state.loading} />
             </div>
         )
     }
